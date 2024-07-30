5d58e7a6c186a82c0869d71653220f0b359a301b
YuuKiSSa <e1241986@u.nus.edu>
2024-07-30 12:28:04 +0800

    VAR

--- PricePrediction/group.py
+++ PricePrediction/group.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pymysql
 
 import category_predict
+from statsmodels.tsa.api import VAR
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
@@ -36,13 +37,75 @@ db.close()
 df_cluster.index.name = 'camera_id'
 df_prices['date'] = pd.to_datetime(df_prices.date)
 df_cluster['release_time'] = pd.to_datetime(df_cluster.release_time)
-df_prices['price_ratio'] = df_prices.apply(lambda row: row['price'] / df_cluster.loc[row['camera_id'] - 1, 'initial_price'], axis=1)
-df_prices['days_after'] = df_prices.apply(lambda row: (row['date'] - df_cluster.loc[row['camera_id'] - 1, 'release_time']).days, axis=1)
+df_prices['price'] = df_prices.apply(lambda row: row['price'] / df_cluster.loc[row['camera_id'] - 1, 'initial_price'], axis=1)
+df_prices['date'] = df_prices.apply(lambda row: (row['date'] - df_cluster.loc[row['camera_id'] - 1, 'release_time']).days, axis=1)
 
-print(df_prices)
+df_prices.to_csv('price/prices.csv', index=False)
 
-group1_price = pd.DataFrame()
-group2_price = pd.DataFrame()
-group3_price = pd.DataFrame()
-group4_price = pd.DataFrame()
+group1_price = pd.DataFrame(columns=['date'])
+group2_price = pd.DataFrame(columns=['date'])
+group3_price = pd.DataFrame(columns=['date'])
+group4_price = pd.DataFrame(columns=['date'])
 
+for camera_id, row in df_cluster.iterrows():
+        camera_id += 1
+        if row['cluster'] == 0:
+                price_now = df_prices[df_prices['camera_id'] == camera_id].copy()
+                if price_now.empty:
+                        continue
+                price_now.drop(columns=['camera_id'], inplace=True)
+                price_now = price_now.rename(columns={'price': camera_id})
+                group1_price = pd.merge(group1_price, price_now, on='date', how='outer')
+        elif row['cluster'] == 1:
+                price_now = df_prices[df_prices['camera_id'] == camera_id].copy()
+                if price_now.empty:
+                        continue
+                price_now.drop(columns=['camera_id'], inplace=True)
+                price_now = price_now.rename(columns={'price': camera_id})
+                group2_price = pd.merge(group2_price, price_now, on='date', how='outer')
+        elif row['cluster'] == 2:
+                price_now = df_prices[df_prices['camera_id'] == camera_id].copy()
+                if price_now.empty:
+                        continue
+                price_now.drop(columns=['camera_id'], inplace=True)
+                price_now = price_now.rename(columns={'price': camera_id})
+                group3_price = pd.merge(group3_price, price_now, on='date', how='outer')
+        elif row['cluster'] == 3:
+                price_now = df_prices[df_prices['camera_id'] == camera_id].copy()
+                if price_now.empty:
+                        continue
+                price_now.drop(columns=['camera_id'], inplace=True)
+                price_now = price_now.rename(columns={'price': camera_id})
+                group4_price = pd.merge(group4_price, price_now, on='date', how='outer')
+
+group1_price.set_index('date', inplace=True)
+group2_price.set_index('date', inplace=True)
+group3_price.set_index('date', inplace=True)
+group4_price.set_index('date', inplace=True)
+group1_price = group1_price.sort_index()
+group2_price = group2_price.sort_index()
+group3_price = group3_price.sort_index()
+group4_price = group4_price.sort_index()
+
+group1_price = group1_price.interpolate(method='linear')
+group2_price = group2_price.interpolate(method='linear')
+group3_price = group3_price.interpolate(method='linear')
+group4_price = group4_price.interpolate(method='linear')
+
+group1_price = group1_price.ffill().bfill()
+group2_price = group2_price.ffill().bfill()
+group3_price = group3_price.ffill().bfill()
+group4_price = group4_price.ffill().bfill()
+
+group1_price.to_csv('price/group1_price.csv', index=True)
+group2_price.to_csv('price/group2_price.csv', index=True)
+group3_price.to_csv('price/group3_price.csv', index=True)
+group4_price.to_csv('price/group4_price.csv', index=True)
+
+#删除常数列
+group1_price_clean = group1_price.drop(columns=[11])
+
+var_model = VAR(group1_price_clean)
+
+results = var_model.fit(maxlags=4)
+print(results.summary())
